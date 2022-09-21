4175af5f635ef84f93b1e9f643d78fc3f30af4e1
Chaiminda Chandrasekara <chaminda.chandrasekara@ifs.com>
2022-09-21 14:14:48 +0200

    deploy agic with aks, kubenet

--- aks/agic_kubenet/main.tf
+++ aks/agic_kubenet/main.tf
@@ -13,14 +13,51 @@ provider "azurerm" {
 
 # resource group for aks
 resource "azurerm_resource_group" "aks_rg" {
-  name     = "rg-aks-dev04"
+  name     = "c"
   location = "westeurope"
 }
 
-# vnet for aks
-resource "azurerm_virtual_network" "aks_vnet" {
-  name                = "vnet_aks_dev04"
-  resource_group_name = azurerm_resource_group.aks_rg.name
+# aks cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+
+  # any autoscaling should not be reset by TF after intial setup
+  lifecycle {
+    ignore_changes = [default_node_pool[0].node_count]
+  }
+
+  name                = "aks-kubenetdemo-dev01"
+  kubernetes_version  = "1.23.8"
   location            = azurerm_resource_group.aks_rg.location
-  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aks-kubenetdemo-dev01-dns"
+  
+
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet"
+  }
+
+  default_node_pool {
+    name                = "demo01linux"
+    enable_auto_scaling = true
+    node_count          = 1
+    min_count           = 1
+    max_count           = 5
+    max_pods            = 110
+    vm_size             = "Standard_B4ms"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  # this will deploy an app gatway in the same vnet
+  # of the AKS cluster and apply user assigned identity
+  # of the AKS cluster for AGIC to the app gateway
+  # so that the app gateway configurations can be managed by the AGIC
+  # based on AKS cluster ingress requirements
+  ingress_application_gateway {
+    gateway_name = "agw-aksingress-demo01"
+    subnet_cidr = "10.225.0.0/24"
+  }
 }
