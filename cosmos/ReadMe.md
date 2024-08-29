e2fcff3655eae83e520f027c7fce73baed8d5458
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2024-08-29 15:48:49 +0200

    simplify

--- cosmos/ReadMe.md
+++ cosmos/ReadMe.md
@@ -1,28 +1,12 @@
-Use `az login` to login to Azure with an SPN
-```
-az login --service-principal --username <spnappid> --password <spnapppwd> --tenant <tenantid>
-az account set --subscription $subscriptionid  
-[Environment]::SetEnvironmentVariable("ARM_CLIENT_ID", <spnappid>)
-[Environment]::SetEnvironmentVariable("ARM_CLIENT_SECRET", <spnapppwd>)
-[Environment]::SetEnvironmentVariable("ARM_SUBSCRIPTION_ID", <subscriptionid>)
-[Environment]::SetEnvironmentVariable("ARM_TENANT_ID", <tenantid>)
-```
-or use `az login` to login to Azure with a user
+Use `az login` to login to Azure with a user
 ```
 az login
 az account set --subscription <subscriptionid> 
 ```
 
-Use below comands with `env.tfvars` and `dev.cfg` updated with correct values for variables and configurations.
+Use below comands run terraform
 ```
-terraform init -backend-config='/backends/dev.cfg'
-terraform plan -var-file='env.tfvars' -out='my.tfplan'
-terraform apply my.tfplan
-```
-
-For local runs with local configs and vars with values (not commited `env.local.tfvars` and `dev.local.cfg`).
-```
-terraform init -backend-config='/backends/dev.local.cfg'
-terraform plan -var-file='env.local.tfvars' -out='my.tfplan'
+terraform init
+terraform plan -out='my.tfplan'
 terraform apply my.tfplan
 ```
--- cosmos/main.tf
+++ cosmos/main.tf
@@ -1,8 +1,50 @@
-resource "azurerm_resource_group" "instance_rg" {
-  name     = "${var.prefix}-${var.project}-${var.env_name}-rg"
-  location = var.region
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.116.0"
+      # version = "=4.0.1"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.53.1"
+    }
+  }
+}
 
-  tags = merge(tomap({
-    Service = "resource group"
-  }), local.tags)
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "instance_rg" {
+  name     = "ch-cosmostest-dev-wus-001-rg"
+  location = "westus"
+}
+
+resource "azurerm_cosmosdb_account" "instancecosmos" {
+  name                          = "ch-cosmostest-dev-wus-001-cdb"
+  location                      = azurerm_resource_group.instance_rg.location
+  resource_group_name           = azurerm_resource_group.instance_rg.name
+  offer_type                    = "Standard"
+  kind                          = "GlobalDocumentDB"
+  automatic_failover_enabled    = false
+  public_network_access_enabled = true
+  # # For azurerm 4.0.1, comment line 10  and uncomment line 11
+  # ip_range_filter = "46.15.109.71,104.42.195.92"
+  # # ip_range_filter                   = ["46.15.109.71/32","104.42.195.92/32"]
+  default_identity_type = "FirstPartyIdentity"
+
+  geo_location {
+    location          = azurerm_resource_group.instance_rg.location
+    failover_priority = 0
+  }
+
+  consistency_policy {
+    consistency_level       = "Session"
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
+  }
 }
