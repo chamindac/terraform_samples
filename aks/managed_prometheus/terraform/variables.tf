efa237900b1b9beb999a6581465ac7d81141b355
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2025-08-01 17:00:10 +0200

    add managed grafana and managed prometheus to aks

--- aks/managed_prometheus/terraform/variables.tf
+++ aks/managed_prometheus/terraform/variables.tf
@@ -2,4 +2,6 @@ locals {
   kubernetes_version = "1.33.2"
   subscription_id    = "subscription_id"
   tenant_id          = "tenant_id"
+  spn_app_id         = "spn_app_id"
+  spn_pwd            = "spn_pwd" # replace with your actual service principal password
 }
